ef149e570037987c809382c1b23672062fe0eafc
cj_developer <cjcontact86@gmail.com>
2021-06-23 19:45:41 +0530

    Added boss level project

---
+++ 4_boss_level/lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Ask Me Anything'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int randomInt = 1;
+
+  void generateRandomNumber() {
+    setState(() {
+      randomInt = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            generateRandomNumber();
+          },
+          child: Image.asset('images/ball$randomInt.png'),
+        ),
+      ),
+    );
+  }
+}
